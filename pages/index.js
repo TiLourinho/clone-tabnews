ac81a1e46a8b447322a91f329d73a8a95898ce7d
Tiago Lourinho <lourinho.tiago@gmail.com>
2024-12-18 16:49:13 -0300

    Change context

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,55 @@
+import styles from "./styles.module.css"
+
 function Home() {
-  return <h1>Hello, World!</h1>
+  return (
+    <main className={styles.main}>
+      <h1>Ballad of Big Nothing</h1>
+      <span>Throwing candy out to the crowd</span>
+      <br />
+      <span>Dragging down the main</span>
+      <br />
+      <span>The helpless little thing with the dirty mouth</span>
+      <br />
+      <span>Who's always got something to say</span>
+      <br />
+      <span>You're sitting around at home now</span>
+      <br />
+      <span>Waiting for your brother to call</span>
+      <br />
+      <span>I saw him down in the alley</span>
+      <br />
+      <span>Having had enough of it all</span>
+      <br />
+      <br />
+      <span>Said you can do what you want to whenever you want to</span>
+      <br />
+      <span>You can do what you want to there's no one to stop you</span>
+      <br />
+      <br />
+      <span>All spit and spite you're up all night and down every day</span>
+      <br />
+      <span>A tired man with only hours to go just waiting to be taken away</span>
+      <br />
+      <span>Getting into the back of a car for candy from some stranger</span>
+      <br />
+      <span>Watching the parade with pinpoint eyes full of smoldering anger</span>
+      <br />
+      <br />
+      <span>You can do what you want to whenever you want to</span>
+      <br />
+      <span>You can do what you want to there's no one to stop you</span>
+      <br />
+      <span>Now you can do what you want to whenever you want to</span>
+      <br />
+      <span>Do what you want to whenever you want to</span>
+      <br />
+      <span>Do what you want to whenever you want to</span>
+      <br />
+      <span>Though it doesn't mean a thing</span>
+      <br />
+      <span>Big nothing</span>
+    </main>
+  )
 }
 
 export default Home;
