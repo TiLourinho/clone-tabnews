792f45e7dc6109ceb109bccd9c4150e98890886c
Tiago Lourinho <lourinho.tiago@gmail.com>
2024-12-19 12:06:05 -0300

    Formatting changes

--- pages/index.js
+++ pages/index.js
@@ -4,50 +4,60 @@ function Home() {
   return (
     <main className={styles.main}>
       <h1>Ballad of Big Nothing</h1>
-      <span>Throwing candy out to the crowd</span>
-      <br />
-      <span>Dragging down the main</span>
-      <br />
-      <span>The helpless little thing with the dirty mouth</span>
-      <br />
-      <span>Who's always got something to say</span>
-      <br />
-      <span>You're sitting around at home now</span>
-      <br />
-      <span>Waiting for your brother to call</span>
-      <br />
-      <span>I saw him down in the alley</span>
-      <br />
-      <span>Having had enough of it all</span>
-      <br />
-      <br />
-      <span>Said you can do what you want to whenever you want to</span>
-      <br />
-      <span>You can do what you want to there's no one to stop you</span>
-      <br />
-      <br />
-      <span>All spit and spite you're up all night and down every day</span>
-      <br />
-      <span>A tired man with only hours to go just waiting to be taken away</span>
-      <br />
-      <span>Getting into the back of a car for candy from some stranger</span>
-      <br />
-      <span>Watching the parade with pinpoint eyes full of smoldering anger</span>
-      <br />
-      <br />
-      <span>You can do what you want to whenever you want to</span>
-      <br />
-      <span>You can do what you want to there's no one to stop you</span>
-      <br />
-      <span>Now you can do what you want to whenever you want to</span>
-      <br />
-      <span>Do what you want to whenever you want to</span>
-      <br />
-      <span>Do what you want to whenever you want to</span>
-      <br />
-      <span>Though it doesn't mean a thing</span>
-      <br />
-      <span>Big nothing</span>
+      <p>
+        Throwing candy out to the crowd
+        <br />
+        Dragging down the main
+        <br />
+        The helpless little thing with the dirty mouth
+        <br />
+        Who's always got something to say
+        <br />
+        You're sitting around at home now
+        <br />
+        Waiting for your brother to call
+        <br />
+        I saw him down in the alley
+        <br />
+        Having had enough of it all
+      </p>
+      <p>
+        Said you can do what you want to whenever you want to
+        <br />
+        You can do what you want to there's no one to stop you
+      </p>
+      <p>
+        All spit and spite you're up all night
+        <br />
+        And down every day
+        <br />
+        A tired man with only hours to go
+        <br />
+        Just waiting to be taken away
+        <br />
+        Getting into the back of a car for
+        <br />
+        Candy from some stranger
+        <br />
+        Watching the parade with pinpoint eyes
+        <br />
+        Full of smoldering anger
+      </p>
+      <p>
+        You can do what you want to whenever you want to
+        <br />
+        You can do what you want to there's no one to stop you
+        <br />
+        Now you can do what you want to whenever you want to
+        <br />
+        Do what you want to whenever you want to
+        <br />
+        Do what you want to whenever you want to
+        <br />
+        Though it doesn't mean a thing
+        <br />
+        Big nothing
+      </p>
     </main>
   )
 }
